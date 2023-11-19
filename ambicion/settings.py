89b86cf153cc92d2a0bfb6a880c10a7e20b24d93
JUN KAKISHIMA <junkakishima@JUNs-MacBook-Pro.local>
2023-11-19 19:42:46 +0900

    fix settings.py

--- ambicion/settings.py
+++ ambicion/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Set Environment Variables
 env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env()
 
 
 # Quick-start development settings - unsuitable for production
